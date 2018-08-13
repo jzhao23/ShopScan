b55f2fc44c0cf69fef5f8e8fa5705a9d88241c6c
jzhao23 <jasonjinzhao99@gmail.com>
2018-08-14 00:58:46 +0900

    added google OCR

---
+++ receipt_scanner/test/test_detect_text.py
@@ -0,0 +1,21 @@
+import io
+import os
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('\n"{}"'.format(texts[0].description))
+
+if __name__ == "__main__":
+    detect_text("/Users/jasonzhao/Documents/GitHub/ShopScan/receipt_scanner/images/receipt.jpg")
